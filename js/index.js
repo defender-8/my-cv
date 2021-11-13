ab1de61ff477f6009fd69cc8ddd5f56f7ee49776
defender-8 <kondratenko.ov.8@gmail.com>
2021-11-13 13:54:08 +0200

    - Accordion

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log('Hola JS!');
+function accordionToggle() {
+  const accordionToggles = document.querySelectorAll('.accordion-toggle');
+
+  accordionToggles.forEach(toggleElem => {
+    toggleElem.addEventListener('click', () => {
+      toggleElem.classList.toggle('active');
+
+      const accordion = toggleElem.closest('.accordion');
+      const accordionSlides = document.querySelectorAll(`#${accordion.id} .accordion-slide`);
+
+      accordionSlides.forEach(slide => {
+        if (slide.id === toggleElem.dataset.slide) {
+          slide.classList.toggle('active');
+        }
+      })
+    })
+  })
+}
+
+accordionToggle();
