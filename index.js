faecfc66951341f33153ec1172798a0e9c806bd7
defender-8 <kondratenko.ov.8@gmail.com>
2021-12-28 18:59:44 +0200

    - Updated structure, progresses and text

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function accordionToggle() {
-  const accordionToggles = document.querySelectorAll('.accordion-toggle');
+  const accordionToggles = document.querySelectorAll('.accordion-item-toggle');
 
   accordionToggles.forEach(toggleElem => {
     toggleElem.addEventListener('click', () => {
       toggleElem.classList.toggle('active');
 
       const accordion = toggleElem.closest('.accordion');
-      const accordionSlides = document.querySelectorAll(`#${accordion.id} .accordion-slide`);
+      const accordionSlides = document.querySelectorAll(`#${accordion.id} .accordion-item-slide`);
 
       accordionSlides.forEach(slide => {
         if (slide.id === toggleElem.dataset.slide) {
